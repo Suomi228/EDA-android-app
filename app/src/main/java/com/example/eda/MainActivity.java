ca27b12f4aac754dbb358138bb906135dfa9a967
Павел Писаренко <bixep345@gmail.com>
2023-09-24 15:15:30 +0300

    addded profile fragment and logout button

--- app/src/main/java/com/example/eda/MainActivity.java
+++ app/src/main/java/com/example/eda/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.eda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button profile_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        profile_btn = findViewById(R.id.profile_btn);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Profile f1 = new Profile();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.container,f1);
+                ft.commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/eda/Profile.java
@@ -0,0 +1,61 @@
+package com.example.eda;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Profile extends Fragment {
+    FirebaseAuth auth;
+    Button logout;
+    TextView user_name;
+    FirebaseUser user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        user_name = (TextView)getView().findViewById(R.id.user_name);
+        logout = (Button) getView().findViewById(R.id.logout);
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getActivity(), Login.class);
+            startActivity(intent);
+        }
+        else{
+            user_name.setText(user.getEmail());
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
