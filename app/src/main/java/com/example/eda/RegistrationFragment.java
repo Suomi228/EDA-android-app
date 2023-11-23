43a66c32cfd7ddaad448a3a56af4c6376059caa2
1.kla$ <recum12233@mail.ru>
2023-11-23 22:10:57 +0300

    пенис 2.0

--- app/src/main/java/com/example/eda/RegistrationFragment.java
+++ app/src/main/java/com/example/eda/RegistrationFragment.java
@@ -19,6 +19,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.eda.databinding.FragmentRegistrationBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -28,14 +29,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationFragment extends FragmentCallback{
 
-    TextInputEditText edit_text_email, edit_text_password, edit_text_confirm;
-    Button button_reg;
+//    TextInputEditText edit_text_email, edit_text_password, edit_text_confirm;
+//    Button button_reg;
     FirebaseAuth mAuth;
-    ProgressBar progress_bar;
-    TextView text_view;
+//    ProgressBar progress_bar;
+//    TextView text_view;
     CallBackFragment callBackFragment;
 
-
+    FragmentRegistrationBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,17 +46,17 @@ public class RegistrationFragment extends FragmentCallback{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+        binding = FragmentRegistrationBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
         mAuth = FirebaseAuth.getInstance();
-        edit_text_email = view.findViewById(R.id.email);
-        edit_text_password = view.findViewById(R.id.password);
-        button_reg = view.findViewById(R.id.btn_register);
-        edit_text_confirm = view.findViewById(R.id.password_confirm);
-        progress_bar = view.findViewById(R.id.progress_bar);
-        text_view = view.findViewById(R.id.loginNow);
+//        edit_text_email = view.findViewById(R.id.email);
+//        edit_text_password = view.findViewById(R.id.password);
+//        button_reg = view.findViewById(R.id.btn_register);
+//        edit_text_confirm = view.findViewById(R.id.password_confirm);
+//        progress_bar = view.findViewById(R.id.progress_bar);
+//        text_view = view.findViewById(R.id.loginNow);
 
-        text_view.setOnClickListener(new View.OnClickListener() {
+        binding.loginNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (callBackFragment!=null){
@@ -63,35 +64,35 @@ public class RegistrationFragment extends FragmentCallback{
                 }
             }
         });
-        button_reg.setOnClickListener(new View.OnClickListener() {
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progress_bar.setVisibility(View.VISIBLE);
+                binding.progressBar.setVisibility(View.VISIBLE);
                 String email,password,password_confirm;
-                email = String.valueOf(edit_text_email.getText());
-                password = String.valueOf(edit_text_password.getText());
-                password_confirm = String.valueOf(edit_text_confirm.getText());
+                email = String.valueOf(binding.email.getText());
+                password = String.valueOf(binding.password.getText());
+                password_confirm = String.valueOf(binding.passwordConfirm.getText());
 
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(password_confirm)){
                     Toast.makeText(getContext(),"Все поля должны быть заполнены!",Toast.LENGTH_SHORT).show();
-                    progress_bar.setVisibility(View.GONE);
+                    binding.progressBar.setVisibility(View.GONE);
                     return;
                 }
                 else if (!password.equals(password_confirm)){
                     Toast.makeText(getContext(),"Пароли не совпадают!",Toast.LENGTH_SHORT).show();
-                    progress_bar.setVisibility(View.GONE);
+                    binding.progressBar.setVisibility(View.GONE);
                     return;
                 }
                 else if (password.length()<8){
                     Toast.makeText(getContext(),"Минимальная длина пароля 8!",Toast.LENGTH_SHORT).show();
-                    progress_bar.setVisibility(View.GONE);
+                    binding.progressBar.setVisibility(View.GONE);
                     return;
                 }
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                progress_bar.setVisibility(View.GONE);
+                                binding.progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
                                     mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
