0d4fe727bc3979738a56695a126811152819647d
Suomi228 <bixep345@gmail.com>
2023-11-25 15:06:52 +0300

    menu crashes need help

--- app/src/main/java/com/example/eda/MenuFragment.java
+++ app/src/main/java/com/example/eda/MenuFragment.java
@@ -2,20 +2,46 @@ package com.example.eda;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.eda.databinding.FragmentMenuBinding;
 
+import java.util.ArrayList;
+
 public class MenuFragment extends FragmentCallback {
     FragmentMenuBinding binding;
+    RecyclerView.Adapter adapter;
+    RecyclerView rec_view_category_list;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        recyclerViewCategoryList();
+    }
+
+    private void recyclerViewCategoryList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, true);
+        binding.recViewMeals.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Salads","category_salads"));
+        category.add(new CategoryDomain("FirstDishes","category_first_dishes"));
+        category.add(new CategoryDomain("SecondDishes","category_second_dishes"));
+        category.add(new CategoryDomain("SideDishes","category_side_dishes"));
+        category.add(new CategoryDomain("Deserts","category_desserts"));
+        category.add(new CategoryDomain("Bakery","category_bakery"));
+        category.add(new CategoryDomain("Pizza","category_pizza"));
+        category.add(new CategoryDomain("Drinks","category_drinks"));
+        adapter = new CategoryAdapter(category);
+        binding.recViewMeals.setAdapter(adapter);
 
     }
 
