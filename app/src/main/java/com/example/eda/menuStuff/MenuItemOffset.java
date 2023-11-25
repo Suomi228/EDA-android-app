17c5364e40d1c042af620aa11b89d85f60d9fb1d
1.kla$ <recum12233@mail.ru>
2023-11-25 17:48:48 +0300

    карточки красива и отступы

---
+++ app/src/main/java/com/example/eda/menuStuff/MenuItemOffset.java
@@ -0,0 +1,22 @@
+package com.example.eda.menuStuff;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MenuItemOffset  extends RecyclerView.ItemDecoration{
+    private int offset;
+
+    public MenuItemOffset(int offset) {
+        this.offset = offset;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.right = offset;
+        outRect.left = offset;
+    }
+}
