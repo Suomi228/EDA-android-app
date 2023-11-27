4c49eb95ed40618a9a84ca35f62ba1db0c61fd48
Suomi228 <bixep345@gmail.com>
2023-11-27 21:16:24 +0300

    Added fragment container to menuFragment

--- app/src/main/java/com/example/eda/CallBackFragment.java
+++ app/src/main/java/com/example/eda/CallBackFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 public interface CallBackFragment {
     void changeFragment(FragmentCallback fragment, boolean allowReturn);
 
+    void changeFragmentCategory(FragmentCallback fragment, boolean allowReturn);
+
     void setNavigationVisibility(int state);
 
 }
